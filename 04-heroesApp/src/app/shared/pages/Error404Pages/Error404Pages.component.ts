465e69daa59c718f9f551613ff4b7d8a808b032d
Dionisos <luisksilvacontreras@gmail.com>
2024-05-16 10:58:58 -0500

    hasta aqui

---
+++ 04-heroesApp/src/app/shared/pages/Error404Pages/Error404Pages.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Error404Pages',
+  templateUrl: './Error404Pages.component.html',
+})
+export class Error404PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ 04-heroesApp/src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { Error404PagesComponent } from './pages/Error404Pages/Error404Pages.component';
+
+
+
+@NgModule({
+  declarations: [Error404PagesComponent],
+  exports: [Error404PagesComponent]
+})
+export class SharedModule { }
